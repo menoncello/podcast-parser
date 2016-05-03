7af5536c09cbbd655e2915dc844a66b744b90924
Eduardo Menoncello <eduardomenoncello@MacBook-Pro-de-Eduardo.local>
2016-05-03 07:11:12 -0300

    fixing the specs

--- tests/execute.specs.js
+++ tests/execute.specs.js
@@ -1,54 +1,52 @@
-(function () {
-  'use strict';
+'use strict';
 
-  let chai = require('chai');
-  let should = chai.should();
-  let sinon = require('sinon');
-  let mockery = require('mockery');
+let chai = require('chai');
+let should = chai.should();
+let sinon = require('sinon');
+let mockery = require('mockery');
 
-  let sandbox = sinon.sandbox.create();
-  let request = function (url, callback) {
-    callback();
-  };
+let sandbox = sinon.sandbox.create();
+let request = function (url, callback) {
+  callback();
+};
 
-  let podcastParser;
+let podcastParser;
 
-  describe('execute', function () {
+describe('execute', function () {
 
-    before(function () {
-      mockery.enable({
-        warnOnReplace: false,
-        warnOnUnregistered: false,
-        useCleanCache: true,
-      });
-    });
-
-    beforeEach(function () {
-      mockery.registerMock('request', request);
-      mockery.registerAllowable('../index.js');
-      mockery.registerAllowable('string');
-      mockery.registerAllowable('xml2js');
-      mockery.registerAllowable('querystring');
-      podcastParser = require('../index.js');
-    });
-
-    afterEach(function () {
-      sandbox.verifyAndRestore();
-      mockery.resetCache();
-      mockery.deregisterAll();
-    });
-
-    after(function () {
-      mockery.disable();
-    });
-
-    it('execute - should return error if url is empty', function (done) {
-
-      podcastParser.execute('', {}, function (err, data) {
-        should.exist(err);
-        done();
-      });
-
+  before(function () {
+    mockery.enable({
+      warnOnReplace: false,
+      warnOnUnregistered: false,
+      useCleanCache: true,
     });
   });
-})();
+
+  beforeEach(function () {
+    mockery.registerMock('request', request);
+    mockery.registerAllowable('../index.js');
+    mockery.registerAllowable('string');
+    mockery.registerAllowable('xml2js');
+    mockery.registerAllowable('querystring');
+    podcastParser = require('../index.js');
+  });
+
+  afterEach(function () {
+    sandbox.verifyAndRestore();
+    mockery.resetCache();
+    mockery.deregisterAll();
+  });
+
+  after(function () {
+    mockery.disable();
+  });
+
+  it('execute - should return error if url is empty', function (done) {
+
+    podcastParser.execute('', {}, function (err, data) {
+      should.exist(err);
+      done();
+    });
+
+  });
+});
